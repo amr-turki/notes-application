23c650d58a32224f6826eaa759d5a87d6981fe15
Amr Turki <amrturki79@gmail.com>
2026-04-10 13:55:59 +0200

    create custom_app_bar

---
+++ lib/widget/custom_app_bar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key, required this.icon});
+
+  final IconData icon;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.2),
+      ),
+      child: IconButton(
+        onPressed: () {
+          if (icon == Icons.close) Navigator.pop(context);
+        },
+        icon: Icon(icon, size: 18),
+      ),
+    );
+  }
+}
