03b84337478ef572a132f2dc94970503eb32d503
Amr Turki <amrturki79@gmail.com>
2026-04-10 14:06:10 +0200

    finish notes view

--- lib/screen/notes_view.dart
+++ lib/screen/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_application/widget/custom_appbar.dart';
+import 'package:notes_application/widget/note_item.dart';
 
 class NotesScreen extends StatelessWidget {
   const NotesScreen({super.key});
@@ -6,6 +8,26 @@ class NotesScreen extends StatelessWidget {
   static String id = "NotesView";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(height: 25),
+            CustomAppbar(text: 'Notes', icon: Icons.search),
+
+            SizedBox(height: 34),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: NoteItem(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
