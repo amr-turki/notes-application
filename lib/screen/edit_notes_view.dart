d3024fae6bd0606043c6475718dc5f8e71546640
Amr Turki <amrturki79@gmail.com>
2026-04-10 14:09:13 +0200

    finish edit notes view

--- lib/screen/edit_notes_view.dart
+++ lib/screen/edit_notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_application/widget/custom_appbar.dart';
+import 'package:notes_application/widget/custom_textfield.dart';
 
 class EditNotesView extends StatelessWidget {
   const EditNotesView({super.key});
@@ -6,6 +8,20 @@ class EditNotesView extends StatelessWidget {
   static String id = "EditNotes";
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(height: 25),
+            CustomAppbar(text: 'Edit Note', icon: Icons.close),
+            SizedBox(height: 34),
+            CustomTextField(text: 'topic'),
+            SizedBox(height: 24),
+            CustomTextField(text: 'content', maxlines: 2),
+          ],
+        ),
+      ),
+    );
   }
 }
