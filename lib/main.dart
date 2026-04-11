77b57736f53157257ded7da26a39f7773f620983
Amr Turki <amrturki79@gmail.com>
2026-04-11 21:36:53 +0200

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_application/constants.dart';
+import 'package:notes_application/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_application/model/note_model.dart';
 import 'package:notes_application/screen/edit_notes_view.dart';
 import 'package:notes_application/screen/notes_view.dart';
@@ -18,15 +20,18 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        NotesScreen.id: (context) => NotesScreen(),
-        ModalSheet.id: (context) => ModalSheet(),
-        EditNotesView.id: (context) => EditNotesView(),
-      },
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => AddNoteCubit())],
+      child: MaterialApp(
+        routes: {
+          NotesScreen.id: (context) => NotesScreen(),
+          ModalSheet.id: (context) => ModalSheet(),
+          EditNotesView.id: (context) => EditNotesView(),
+        },
 
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(),
+      ),
     );
   }
 }
